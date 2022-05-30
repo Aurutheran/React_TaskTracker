8a8479d50a1977793ec8a9bfefaaf914fba56025
Aurutheran <74684383+Aurutheran@users.noreply.github.com>
2022-05-30 00:22:27 -0400

    Uploading the base files for React Application.
    The following contains the initial files we will be altering for our React JS Task Tracker Application.

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import PropTypes from 'prop-types'
+
+const Header = ({title}) => {
+  return (
+    <header>
+        <h1 style>{title}</h1>
+    </header>
+  )
+}
+
+Header.defaultProps ={
+    title: 'Task Tracker',
+}
+
+Header.propTypes ={
+    title: PropTypes.string.isRequired,
+}
+
+//CSS IN JS
+// const headingStyle = { 
+//     color: 'red', 
+//     backgroundColor: 'black'
+// }
+export default Header
