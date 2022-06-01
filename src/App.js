a7cc84699b6ba9817dd6de9aa5baf6732e12e5fb
Aurutheran <74684383+Aurutheran@users.noreply.github.com>
2022-06-01 02:04:05 -0400

    Removing Tasks Functionality
    Added functionality to remove tasks that  were pre-defined. Next update will consist of being able to add manual tasks.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
+
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30PM',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 6th at 1:30PM',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30PM',
+        reminder: false,
+    }
+])
+
+//Delete Task
+const deleteTask = (id) =>{
+  setTasks(tasks.filter( (task) => task.id !== id))
+}
   return (
     <div className='container'>
       <Header title={'Task Tracker'} />
+      <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   )
 }
